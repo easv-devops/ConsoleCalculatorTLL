325dfd358b890304e16072dcbfb3e50a2fe8b8ac
Thomas <tlloevkilde@gmail.com>
2024-02-16 13:18:50 +0100

    removed code

--- CalculatorTests/UnitTest1.cs
+++ CalculatorTests/UnitTest1.cs
@@ -5,77 +5,77 @@ namespace CalculatorTests
 {
     public class CalculatorTests
     {
-        [Fact]
-        public void Add_ShouldReturnCorrectSum()
-        {
-            // Arrange
-            Calculator calculator = new Calculator();
-            double num1 = 5;
-            double num2 = 3;
+        // [Fact]
+        // public void Add_ShouldReturnCorrectSum()
+        // {
+        //     // Arrange
+        //     Calculator calculator = new Calculator();
+        //     double num1 = 5;
+        //     double num2 = 3;
 
 
-            // Act
-            double result = calculator.Add(num1, num2);
+        //     // Act
+        //     double result = calculator.Add(num1, num2);
 
-            // Assert
-            Assert.Equal(8, result);
-        }
+        //     // Assert
+        //     Assert.Equal(8, result);
+        // }
 
-        [Fact]
-        public void Subtract_ShouldReturnCorrectDifference()
-        {
-            // Arrange
-            Calculator calculator = new Calculator();
-            double num1 = 10;
-            double num2 = 4;
+        // [Fact]
+        // public void Subtract_ShouldReturnCorrectDifference()
+        // {
+        //     // Arrange
+        //     Calculator calculator = new Calculator();
+        //     double num1 = 10;
+        //     double num2 = 4;
 
-            // Act
-            double result = calculator.Subtract(num1, num2);
+        //     // Act
+        //     double result = calculator.Subtract(num1, num2);
 
-            // Assert
-            Assert.Equal(6, result);
-        }
+        //     // Assert
+        //     Assert.Equal(6, result);
+        // }
 
-        [Fact]
-        public void Multiply_ShouldReturnCorrectProduct()
-        {
-            // Arrange
-            Calculator calculator = new Calculator();
-            double num1 = 7;
-            double num2 = 2;
+        // [Fact]
+        // public void Multiply_ShouldReturnCorrectProduct()
+        // {
+        //     // Arrange
+        //     Calculator calculator = new Calculator();
+        //     double num1 = 7;
+        //     double num2 = 2;
 
-            // Act
-            double result = calculator.Multiply(num1, num2);
+        //     // Act
+        //     double result = calculator.Multiply(num1, num2);
 
-            // Assert
-            Assert.Equal(14, result);
-        }
+        //     // Assert
+        //     Assert.Equal(14, result);
+        // }
 
-        [Fact]
-        public void Divide_ShouldReturnCorrectQuotient()
-        {
-            // Arrange
-            Calculator calculator = new Calculator();
-            double num1 = 15;
-            double num2 = 3;
+        // [Fact]
+        // public void Divide_ShouldReturnCorrectQuotient()
+        // {
+        //     // Arrange
+        //     Calculator calculator = new Calculator();
+        //     double num1 = 15;
+        //     double num2 = 3;
 
-            // Act
-            double result = calculator.Divide(num1, num2);
+        //     // Act
+        //     double result = calculator.Divide(num1, num2);
 
-            // Assert
-            Assert.Equal(5, result);
-        }
+        //     // Assert
+        //     Assert.Equal(5, result);
+        // }
 
-        [Fact]
-        public void Divide_ByZero_ShouldThrowArgumentException()
-        {
-            // Arrange
-            Calculator calculator = new Calculator();
-            double num1 = 10;
-            double num2 = 0;
+        // [Fact]
+        // public void Divide_ByZero_ShouldThrowArgumentException()
+        // {
+        //     // Arrange
+        //     Calculator calculator = new Calculator();
+        //     double num1 = 10;
+        //     double num2 = 0;
 
-            // Act & Assert
-            Assert.Throws<ArgumentException>(() => calculator.Divide(num1, num2));
-        }
+        //     // Act & Assert
+        //     Assert.Throws<ArgumentException>(() => calculator.Divide(num1, num2));
+        // }
     }
 }
--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -11,22 +11,6 @@ namespace ConsoleCalculator
             // double resultAddition = calculator.Add(5, 3);
             // Console.WriteLine("Addition result: " + resultAddition);
 
-            // double resultSubtraction = calculator.Subtract(10, 4);
-            // Console.WriteLine("Subtraction result: " + resultSubtraction);
-
-            // double resultMultiplication = calculator.Multiply(7, 2);
-            // Console.WriteLine("Multiplication result: " + resultMultiplication);
-
-            // try
-            // {
-            //     double resultDivision = calculator.Divide(15, 3);
-            //     Console.WriteLine("Division result: " + resultDivision);
-            // }
-            // catch (ArgumentException ex)
-            // {
-            //     Console.WriteLine("Error: " + ex.Message);
-            // }
-
             // Console.ReadLine(); 
         }
     }
