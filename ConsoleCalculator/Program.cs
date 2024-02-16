3ce86a595c01f7d201a33c0fcd105eedf192efab
Thomas <tlloevkilde@gmail.com>
2024-02-16 12:50:44 +0100

    Running tests with sonar scanner

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -8,18 +8,18 @@ namespace ConsoleCalculator
         {
             Calculator calculator = new Calculator();
 
-            double resultAddition = Calculator.Add(5, 3);
+            double resultAddition = calculator.Add(5, 3);
             Console.WriteLine("Addition result: " + resultAddition);
 
-            double resultSubtraction = Calculator.Subtract(10, 4);
+            double resultSubtraction = calculator.Subtract(10, 4);
             Console.WriteLine("Subtraction result: " + resultSubtraction);
 
-            double resultMultiplication = Calculator.Multiply(7, 2);
+            double resultMultiplication = calculator.Multiply(7, 2);
             Console.WriteLine("Multiplication result: " + resultMultiplication);
 
             try
             {
-                double resultDivision = Calculator.Divide(15, 3);
+                double resultDivision = calculator.Divide(15, 3);
                 Console.WriteLine("Division result: " + resultDivision);
             }
             catch (ArgumentException ex)
